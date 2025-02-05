5cfdca9adfa76a95b12c14a0c5d937d98529923f
AVMihel <dok2000@list.ru>
2025-02-05 12:45:04 +0500

    two

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Бонусы авиаперевозчика");
+        System.out.println("БОНУСЫ АВИАПЕРЕВОЗЧИКА");
         int price;
         price = 13_676;
         int b = 20;
-        System.out.println("Цена авиабилета");
+        System.out.println("Цена авиабилета:");
         System.out.println(price);
         int m = price / b;
 
