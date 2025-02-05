7d6570612f10addaf0bc0ef154cc28951573d1b5
AVMihel <dok2000@list.ru>
2025-02-05 12:47:04 +0500

    three

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("БОНУСЫ АВИАПЕРЕВОЗЧИКА");
         int price;
-        price = 13_676;
+        price = 7_635;
         int b = 20;
         System.out.println("Цена авиабилета:");
         System.out.println(price);
