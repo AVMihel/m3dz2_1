c5ecd9544c9a6c4eab29164fdea80576388e06c1
AVMihel <dok2000@list.ru>
2025-02-06 21:25:56 +0500

    six

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("БОНУСЫ АВИАПЕРЕВОЗЧИКА");
-        int amount;
-        amount = 7_635;
+        int amount = 7_635;
         int priceMile = 20;
         System.out.println("Цена авиабилета: " + amount);
-        int m = amount / priceMile;
-        System.out.println("Начислено миль: " + m);
+        int miles = amount / priceMile;
+        System.out.println("Начислено миль: " + miles);
     }
 }
